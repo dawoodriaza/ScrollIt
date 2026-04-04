d7c7289c99b039195d354fb576c3d19fa5fc1100
dawoodriaza <od.riaz@alsalambank.com>
2026-04-04 15:13:01 +0300

    Added changes

---
+++ src/test/java/com/livestream/livestream_api/LivestreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livestream.livestream_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivestreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
