38b8cea65432b1775c47669c598967c66ad5780c
dawoodriaza <od.riaz@alsalambank.com>
2026-04-05 19:43:15 +0300

    UnauthorizedException

---
+++ src/main/java/com/livestream/livestream_api/exception/UnauthorizedException.java
@@ -0,0 +1,12 @@
+package com.livestream.livestream_api.exception;
+
+
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+
+        super(message);
+
+
+    }
+}
